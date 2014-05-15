63ec2b33129f5f4f017f082b8e0904441c45e707
Franck KALB <fkalb@e2-cdi-p1.42.fr>
2014-05-15 03:08:07 +0200

    Autopush - Thu May 15 03:08:06 CEST 2014

--- java/fr/ironcraft/phonecraft/client/KeyHandler.java
+++ java/fr/ironcraft/phonecraft/client/KeyHandler.java
@@ -7,16 +7,21 @@ import org.lwjgl.input.Keyboard;
 import cpw.mods.fml.client.registry.ClientRegistry;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.InputEvent.KeyInputEvent;
+import cpw.mods.fml.common.registry.LanguageRegistry;
 
 public class KeyHandler {
 	
-	private KeyBinding key_PhoneGUI  = new KeyBinding("Phone", Keyboard.KEY_P, "?");
+	private KeyBinding key_PhoneGUI  = new KeyBinding(LanguageRegistry.instance().getStringLocalization("key.phone.open", "en_US"), Keyboard.KEY_P, "Phonecraft");
 	public static boolean open_phoneGUI = false;
 
 	public KeyHandler()
 	{
 	    ClientRegistry.registerKeyBinding(key_PhoneGUI);
 	    System.out.println("Keys was registered");
+	    
+	    System.out.println("TEST ----------------------------- "+LanguageRegistry.instance().getStringLocalization("key.phone.open", "en_US"));
+	    System.out.println(LanguageRegistry.instance().getStringLocalization("key.phone.open", "fr_FR"));
+	    System.out.println(LanguageRegistry.instance().getStringLocalization("key.phone.open", "fr_CA"));
 	}
 
 	@SubscribeEvent
