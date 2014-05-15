a6d9f8719cc28db46d7b9bcf9a10964d38e69cdd
Franck KALB <fkalb@e3r12p18.42.fr>
2014-05-15 09:29:03 +0200

    Autopush - Thu May 15 09:28:56 CEST 2014

---
+++ java/fr/ironcraft/phonecraft/client/gui/GuiPhoneCamera.java
@@ -0,0 +1,11 @@
+package fr.ironcraft.phonecraft.client.gui;
+
+import net.minecraft.client.Minecraft;
+
+public class GuiPhoneCamera extends GuiPhoneInGame {
+
+	public GuiPhoneCamera(Minecraft par1Minecraft) {
+		super(par1Minecraft);
+	}
+
+}
---
+++ java/fr/ironcraft/phonecraft/client/gui/GuiPhoneMessages.java
@@ -0,0 +1,11 @@
+package fr.ironcraft.phonecraft.client.gui;
+
+import net.minecraft.client.Minecraft;
+
+public class GuiPhoneMessages extends GuiPhoneInGame {
+
+	public GuiPhoneMessages(Minecraft par1Minecraft) {
+		super(par1Minecraft);
+	}
+
+}
