ef7afa0322a4503438dcb8fedaeb29a19cb34a73
Franck KALB <fkalb@e2-cdi-p1.42.fr>
2014-05-15 07:02:00 +0200

    Autopush - Thu May 15 07:01:57 CEST 2014

--- java/fr/ironcraft/phonecraft/client/gui/GuiPhoneAnimation.java
+++ java/fr/ironcraft/phonecraft/client/gui/GuiPhoneAnimation.java
@@ -6,40 +6,36 @@ import net.minecraft.client.gui.GuiScreen;
 import fr.ironcraft.phonecraft.client.ClientProxy;
 import fr.ironcraft.phonecraft.client.KeyHandler;
 
-public class GuiPhone extends GuiPhoneInGame {
+public class GuiPhoneAnimation extends GuiPhoneInGame {
 	
-	public GuiPhone(Minecraft par1Minecraft, boolean isOpen)
+	public GuiPhoneAnimation(Minecraft par1Minecraft, boolean open)
 	{
 		super(par1Minecraft);
-		this.isOpen = isOpen;
-		this.shift = this.isOpen ? 214 : 0;
+		this.isOpen = !open;
+		this.shift = !open ? 214 : 0;
+		this.isAnimated = true;
 		mc.thePlayer.triggerAchievement(ClientProxy.achievements.openPhone);
 	}
 	
+	@Override
 	public void drawScreen(int par1, int par2, float par3)
 	{
 		super.drawScreen(par1, par2, par3);
-		this.drawRect(this.width - 106 + this.shift, this.height - 183, this.width - 14 + this.shift, this.height - 29, 0xff000000);
 		drawAnimation();
 	}
-	
-	@Override
-	protected void keyTyped(char par1, int par2)
-    {
-		if (par2 == 1) {
-			this.mc.displayGuiScreen(new GuiIngameMenu());
-		}
-        if (par2 == KeyHandler.key_PhoneGUI.getKeyCode())
-        	this.isOpen = false;
-    }
 	
     private void drawAnimation()
     {
     	if (this.isOpen && this.shift != 0)
     		this.shift = this.shift - 5 > 4 ? this.shift - 10 : 0;
+    	else if (this.isOpen && this.shift == 0) {
+    		this.isAnimated = false;
+    		this.mc.displayGuiScreen(new GuiPhoneMenu(this.mc));
+    	}
     	else if (!this.isOpen) {
     		this.shift = this.shift + 5 < 210 ? this.shift + 10 : 214;
     		if (this.shift >= 214) {
+    			this.isAnimated = false;
     			this.mc.displayGuiScreen((GuiScreen)null);
     			this.mc.setIngameFocus();
     		}
---
+++ java/fr/ironcraft/phonecraft/client/gui/GuiPhoneMenu.java
@@ -0,0 +1,15 @@
+package fr.ironcraft.phonecraft.client.gui;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiIngameMenu;
+import fr.ironcraft.phonecraft.client.ClientProxy;
+import fr.ironcraft.phonecraft.client.KeyHandler;
+
+public class GuiPhoneMenu extends GuiPhoneInGame {
+
+	public GuiPhoneMenu(Minecraft par1Minecraft)
+	{
+		super(par1Minecraft);
+	}
+	
+}
