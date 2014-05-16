2eda0b4970fd25eb6e8403935a4ce14ac253c1fb
Franck KALB <fkalb@e3r7p21.42.fr>
2014-05-16 08:39:59 +0200

    optimisation - Fri May 16 08:39:57 CEST 2014

--- java/fr/ironcraft/phonecraft/client/ClientProxy.java
+++ java/fr/ironcraft/phonecraft/client/ClientProxy.java
@@ -1,19 +1,30 @@
 package fr.ironcraft.phonecraft.client;
 
+import net.minecraft.client.Minecraft;
 import net.minecraftforge.common.MinecraftForge;
 import cpw.mods.fml.common.FMLCommonHandler;
 import fr.ironcraft.phonecraft.common.CommonProxy;
+import fr.ironcraft.phonecraft.utils.CustomFont;
 import fr.ironcraft.phonecraft.utils.ImageLoader;
 
 public class ClientProxy extends CommonProxy {
 
+	private static Minecraft mc = Minecraft.getMinecraft();
 	public static PhoneAchievements achievements;
+<<<<<<< Updated upstream
 	public static ImageLoader imageLoader = new ImageLoader();
+=======
+	public static ImageLoader imageLoader;
+	public static CustomFonts fonts;
+>>>>>>> Stashed changes
 
 	public void init()
 	{
 		events();
 		achievements = new PhoneAchievements();
+		fonts = new CustomFonts();
+		
+		fonts.init();
 	}
 	
 	public void renders()
